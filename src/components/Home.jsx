51fd2568e2f57afacf04e1d43ee7c33e239e5c4b
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2025-06-14 19:27:06 +0000

    DSA

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import '../index.css'; // Create a corresponding CSS file for styling
+import React from 'react';
+import '../index.css'; // Ensure this file contains the styles
 
 const Home = () => {
   return (
     <div className="home">
+      <div className="bubble"></div>
         Hello World
     </div>
   );
