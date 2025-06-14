86702f6703d4d9ad012b03a12ff6d78d765c0f37
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2025-06-14 22:10:51 +0000

    DSA

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ import '../index.css'; // Ensure this file contains the styles
 const Home = () => {
   return (
     <div className="home">
-      <img src="/workspaces/Data-Structure-Algorithm/public/vite.svg" alt="" srcset="" />
-        
+      Data Structure and 
+        <span> Algorithm</span>
     </div>
   );
 };
